c34243db499e7563b0aa82d55eb0d4117cec7fc0
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-13 14:01:48 +0100

    feat: Remove outdated objectFit attribute from Image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 import src from "../assets/start_icon.png";
 import { useState } from "react";
@@ -9,16 +10,15 @@ const Home = () => {
 	const [start, setStart] = useState(false);
 
 	return (
-		<div className={`${styles.welcomeWrapper} ${start ? styles.hidden : ""}`}>
-			<Image
-				alt="welcome icon"
-				src={src}
-				width={200}
-				height={200}
-			/>
-			<h1 className={styles.typewriter}>Faro</h1>
-			<h1>Weather App</h1>
-			<button onClick={() => setStart(true)}>Get Info</button>
+		<div
+			className={`${styles.welcomeWrapper} ${start ? styles.hidden : ""}`}
+		>
+			<Image alt="welcome icon" src={src} width={200} height={200} />
+			<h1 className={styles.typewriter}>Portugal</h1>
+			<h1>Weather</h1>
+			<Link href="/data" onClick={() => setStart(true)}>
+				Entrar
+			</Link>
 		</div>
 	);
 };
