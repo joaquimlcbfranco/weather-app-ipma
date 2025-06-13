cc608676d547bb4edeeee0d04bdb362d21a38f15
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-13 10:51:43 +0100

    feat: Create state variable to check wether user has clicked the start button or not

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,24 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
-import src from "../assets/start_icon.png"
+import src from "../assets/start_icon.png";
+import { useState } from "react";
 
 const Home = () => {
+	const [start, setStart] = useState(false);
+
 	return (
-		<div className={styles.welcomeWrapper}>
-      <Image src={src} width={200} height={200} objectFit="cover" />
-			<h1>Faro</h1>
+		<div className={`${styles.welcomeWrapper} ${start ? styles.hidden : ""}`}>
+			<Image
+				alt="welcome icon"
+				src={src}
+				width={200}
+				height={200}
+			/>
+			<h1 className={styles.typewriter}>Faro</h1>
 			<h1>Weather App</h1>
-			<button>Get Started</button>
+			<button onClick={() => setStart(true)}>Get Info</button>
 		</div>
 	);
 };
