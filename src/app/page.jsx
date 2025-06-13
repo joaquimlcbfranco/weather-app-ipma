a8dda862e189e443efcdfc5f76aaee2f9d724750
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-13 10:06:27 +0100

    feat: Add image, heading and button to welcome wrapper

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import src from "../assets/start_icon.png"
 
-export default function Home() {
-  return (
-    <></>
-  );
-}
+const Home = () => {
+	return (
+		<div className={styles.welcomeWrapper}>
+      <Image src={src} width={200} height={200} objectFit="cover" />
+			<h1>Faro</h1>
+			<h1>Weather App</h1>
+			<button>Get Started</button>
+		</div>
+	);
+};
+
+export default Home;
